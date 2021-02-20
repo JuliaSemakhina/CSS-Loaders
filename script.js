69d1390fc9a28320506be243344c339125ee0cc2
Julia Semakhina <contremoi@yandex.ru>
2021-02-20 19:16:34 +0300

    final files

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let wrapper = document.getElementById('wrapper');
-    let topLayer = wrapper.querySelector('.top');
-    let handle = wrapper.querySelector('.handle');
-    let skew = 0;
-    let delta = 0;
+const loader = document.querySelector('.loader');
+const main = document.querySelector('.main');
 
+function init(){
+    setTimeout(()=>{
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
 
-    if (wrapper.className.indexOf('skewed') != -1) {
-        skew = 990;
-    }
+        main.style.display = "block";
+        setTimeout(()=> main.style.opacity = 1, 50);
+    }, 4000);
+}
 
-    wrapper.addEventListener('mousemove', function(e) {
-        delta = (e.clientX - window.innerWidth / 2) * 0.5;
-
-        handle.style.left = e.clientX + delta + "px";
-
-        topLayer.style.width = e.clientX + skew + delta + "px";
-    })
-
-
-});
+init();
